c2cc4e47971dcd291bcdf0ed80af11665a37dc30
AyushDeuja <ayushdeuja11@gmail.com>
2025-01-08 17:52:19 +0545

    part-01

---
+++ App.js
@@ -0,0 +1,24 @@
+// const heading = React.createElement(
+//   "h1",
+//   {id: "heading"},
+//   "Hello world from react, It's me AYUSH"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "Hello react I am an h1 tag"),
+        React.createElement("h2", {}, "Hello react I am an h2 tag"),
+      ]),
+      React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "Hello react I am an h1 tag"),
+        React.createElement("h2", {}, "Hello react I am an h2 tag"),
+      ])
+  ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
